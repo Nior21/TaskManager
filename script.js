42085957e282127e1ca0b859a56e91ed8294bfcf
Nior21 <lhall@ya.ru>
2021-09-07 03:23:08 +0300

    Added new ul selectors for the distribution of li classes.

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function handlerClick () {
 
     let input$ = document.createElement("input");
     input$.id = "input";
-    input$.defaultValue = "Create new task...";
 
     document.querySelector("#add").textContent = "";
     document.querySelector ( "#add").appendChild(input$);
+    input$.focus();
 
     input$.addEventListener("keydown", handlerKeyDown);
 
@@ -15,23 +15,34 @@ function handlerClick () {
 }
 
 function handlerKeyDown(e) {
-    if (e.keyCode == 13) {
+    if (e.keyCode === 13) {
         console.log("handlerKeyDown")
 
         document.querySelector ( "#add" ).textContent = e.target.value;
 
         document.querySelector("#add").removeEventListener("keydown", handlerKeyDown);
+        document.querySelector("#add").addEventListener("click", handlerClickOnTask);
 
         document.querySelector ( "#add" ).id = "";
 
         let newAdd$ = document.createElement("li");
-        newAdd$.textContent = "Create new task...";
+        newAdd$.textContent = "Click to create a new task...";
         newAdd$.id = "add";
         document.querySelector("ul").appendChild(newAdd$);
+
         newAdd$.addEventListener("click", handlerClick);
     }
 }
 
 let add$ = document.querySelector ( "#add" );
 add$.addEventListener("click", handlerClick);
+
+function handlerClickOnTask(e) {
+    if (e.target.className === "del" || e.target.className === undefined) {
+        e.target.className = "";
+    } else {
+        e.target.className = "del"
+    }
+
+}
 
